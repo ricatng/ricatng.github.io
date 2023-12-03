860e6864c484db636324c3539f0a959be4f9284f
ricatng <152774262+ricatng@users.noreply.github.com>
2023-12-03 15:55:43 +0700

    Add files via upload

---
+++ untitled.java
@@ -0,0 +1,29 @@
+const wrapper document.querySelector(".wrapper"); const question document.querySelector(".question");
+
+const gif document.querySelector(".gif");
+
+const yesBtn = document.querySelector(".yes-btn"); const noßtn document.querySelector(*.no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question, InnerHTML "lia, besok mau bareng ga?";
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+gif.src =
+
+noBtn.addEventListener("mouseover", () => {
+
+const noßtnRect noßtn.getBoundingClientRect();
+
+const maxx window.innerWidth noßtnRect.width; const maxy window.innerHeight noBtnRect.height;
+
+const randomX = Math.floor(Math.random() maxx);
+
+const randomy Math.floor(Math.random() maxY); randomX + "px";
+
+CodeSphere
+
++
+
+noßtn.style.left nobtn.style.top }); randomy+"px";
